2fce452dd3517b9d7032597b82b4a0599a0e487f
harrytran998 <nhattq.coding@gmail.com>
2024-11-06 07:35:14 +0700

    feat: bundle lib to cjs,esm

--- build.ts
+++ build.ts
@@ -1,29 +1,44 @@
 import { execSync } from "node:child_process";
 import json from "./package.json";
+import type { BuildConfig } from "bun";
 
 execSync("rm -rf dist");
-await Bun.build({
+
+const buildConfig: BuildConfig = {
   entrypoints: [
     "src/index.ts",
-    "src/+config.ts",
-    "src/components/clientOnly.tsx",
-    "src/components/withFallback.tsx",
-    "src/components/FallbackErrorBoundary.tsx",
-    "src/providers/ReactQueryProvider.tsx",
-    "src/query/+config.ts",
-    "src/hooks/useData.ts",
-    "src/hooks/usePageContext.ts",
-    "src/render/onRenderClient.tsx",
-    "src/render/onRenderHtml.tsx",
+    "src/domain/aggregate/index.ts",
+    "src/domain/context/index.ts",
+    "src/domain/entity/index.ts",
+    "src/domain/entity/unique-entity.ts",
+    "src/domain/events/index.ts",
+    "src/domain/value-object/index.ts",
+    "src/domain/repository.ts",
+    "src/domain/use-cases-port.ts",
+    "src/infra/index.ts",
+    "src/utils/index.ts",
   ],
   outdir: "dist",
-  target: "bun",
-  sourcemap: "inline",
-  external: [...Object.keys(json.peerDependencies)],
+  format: "cjs",
+  target: "node",
+  external: [
+    ...Object.keys(json.dependencies),
+    ...Object.keys(json.devDependencies),
+  ],
+};
+
+await Bun.build({
+  ...buildConfig,
+  naming: "[dir]/[name].js",
 });
 
-execSync("bun run tsc --emitDeclarationOnly --outDir dist");
+await Bun.build({
+  ...buildConfig,
+  format: "esm",
+  naming: "[dir]/[name].mjs",
+});
+
+execSync("bun tsc --emitDeclarationOnly --outDir dist");
 execSync("mv dist/src/* dist");
-execSync("cp src/typing.d.ts dist");
-execSync("cp src/typing.d.ts dist");
 execSync("rm -rf dist/tsconfig.tsbuildinfo");
+execSync("rm -rf dist/src");
